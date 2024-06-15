c9c5c020184654d203ba70889173066106ef6216
TranHBach <ragoza0932447933@gmail.com>
2024-06-15 23:21:26 +0700

    Add JDBC comment

--- postgresAdapter.java
+++ postgresAdapter.java
@@ -3,6 +3,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class postgresAdapter {
+    // The Java Database connectivity (JDBC) driver 
+    // specify connection to Postgres with this URL
+    // user and password is set in Postgres
     private final String url = "jdbc:postgresql://localhost:5433/postgres";
     private final String user = "postgres";
     private final String password = "123";
@@ -10,6 +13,7 @@ public class postgresAdapter {
     public Connection connect() throws SQLException {
         Connection conn = null;
         try {
+            // DriverManager get method from JDBC driver
             conn = DriverManager.getConnection(url, user, password);
         } catch (SQLException e) {
             System.out.println(e);
